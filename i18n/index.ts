89fde2ce3b053d714794980add1722320c27ac19
karlosvas <carlosvassan@gmail.com>
2025-08-11 20:25:50 +0200

    chore: first commit

---
+++ i18n/index.ts
@@ -0,0 +1,31 @@
+import english from "./en.json";
+import spanish from "./es.json";
+import french from "./fr.json";
+import german from "./de.json";
+import italian from "./it.json";
+import portuguese from "./pt.json";
+import arabic from "./ar.json";
+import type { i18nTranslations } from "@/types/types";
+
+// Lenguajes permitidos
+const enum Languages {
+  ENGLISH = "en",
+  SPANISH = "es",
+  FRENCH = "fr",
+  GERMAN = "de",
+  ITALIAN = "it",
+  PORTUGUESE = "pt",
+  ARABIC = "ar",
+}
+
+// Obtener la traduccion correspondiente al idioma selecionado
+export const getI18N = ({ lang = "es" }: { lang: string | undefined }): i18nTranslations => {
+  if (lang === Languages.ENGLISH) return english;
+  else if (lang === Languages.SPANISH) return spanish;
+  else if (lang === Languages.FRENCH) return french;
+  else if (lang === Languages.GERMAN) return german;
+  else if (lang === Languages.ITALIAN) return italian;
+  else if (lang === Languages.PORTUGUESE) return portuguese;
+  else if (lang === Languages.ARABIC) return arabic;
+  else return spanish;
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+export const content = [
+    "./src/**/*.{astro,js,ts,jsx,tsx}"
+];
+export const theme = {
+    extend: {
+        colors: {
+            background: 'var(--color-white)',
+            text: 'var(--color-black)',
+        },
+    }
+};
+export const plugins = [];
