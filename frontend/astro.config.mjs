e9f716f59c544453be7aa489d2b6e8353eeb4763
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-08-25 16:30:28 +0200

    feat: login form and better responsiveness

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amanahacademia.com',
+  output: 'static',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'fr', 'de', 'it', 'pt', 'ar'],
